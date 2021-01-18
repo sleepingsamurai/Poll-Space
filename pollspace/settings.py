402ab5d43ca93b52c439234106fee32b7d662ff3
Anubhav <anubhav.singh.met19@itbhu.ac.in>
2021-01-19 02:24:47 +0530

    second

--- pollspace/settings.py
+++ pollspace/settings.py
@@ -121,4 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_URL = '/images/'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
